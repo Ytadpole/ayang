e71932bc306ed7f320721007bf65724476beb7a8
ayang <819375546@qq.com>
2018-02-22 19:30:22 +0800

    index

---
+++ src/main/java/com/yangs/service/IContentService.java
@@ -0,0 +1,7 @@
+package com.yangs.service;
+
+/**
+ * Created by Ytadpole on 2018/2/22.
+ */
+public class IContentService {
+}
---
+++ src/main/java/com/yangs/service/impl/ContentService.java
@@ -0,0 +1,7 @@
+package com.yangs.service.impl;
+
+/**
+ * Created by Ytadpole on 2018/2/22.
+ */
+public class ContentService {
+}
