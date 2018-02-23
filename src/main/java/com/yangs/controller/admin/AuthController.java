2d704f221aa9a0c50f6db2b0d4d121a759d8a069
ayang <819375546@qq.com>
2018-02-23 20:27:16 +0800

    登录界面

---
+++ src/main/java/com/yangs/controller/admin/AuthController.java
@@ -0,0 +1,7 @@
+package com.yangs.controller.admin;
+
+/**
+ * Created by Ytadpole on 2018/2/23.
+ */
+public class AuthController {
+}
---
+++ src/main/java/com/yangs/utils/IPUtil.java
@@ -0,0 +1,7 @@
+package com.yangs.utils;
+
+/**
+ * Created by Ytadpole on 2018/2/23.
+ */
+public class IPUtil {
+}
